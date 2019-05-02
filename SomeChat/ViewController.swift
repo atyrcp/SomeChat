320afeba08a29392f5b0a2c3e9530e70412bc136
atyrcp <atyrcp@gmail.com>
2019-05-02 21:50:00 +0800

    Initial Commit

---
+++ SomeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SomeChat
+//
+//  Created by alien on 2019/5/2.
+//  Copyright © 2019 z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
