acf5fa98936949d42b6650b43717142ede080a93
atyrcp <atyrcp@gmail.com>
2019-05-04 15:27:19 +0800

    dragged in the fireStore info plist, and modified the chat label

--- SomeChat/ViewController.swift
+++ SomeChat/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var chatRoomScrollView: UIScrollView!
+    @IBOutlet weak var nickNameTextField: UITextField!
+    @IBOutlet weak var chatMessageTextField: UITextField!
+    
+    var labelHeight: CGFloat = 0
+    
+    @IBAction func sendMessage(_ sender: UIButton) {
+        guard let name = nickNameTextField.text, let message = chatMessageTextField.text else {return}
+        let messageLabel = UILabel()
+        messageLabel.text = name + "\n" + message
+        messageLabel.numberOfLines = 0
+        messageLabel.frame = CGRect(x: 0, y: labelHeight, width: chatRoomScrollView.frame.width, height: messageLabel.frame.height)
+        messageLabel.sizeToFit()
+        chatRoomScrollView.addSubview(messageLabel)
+        labelHeight += messageLabel.frame.height + 8
+        
+        nickNameTextField.text = nil
+        chatMessageTextField.text = nil
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
